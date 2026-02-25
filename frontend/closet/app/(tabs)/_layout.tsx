c4f23567cd0c8e7693a0b5b6c4869cd23fe440b0
ramyysameh <117130865+ramyysameh@users.noreply.github.com>
2026-02-25 14:05:40 +0200

    Rename tab from 'community' to 'Community'

--- frontend/closet/app/(tabs)/_layout.tsx
+++ frontend/closet/app/(tabs)/_layout.tsx
@@ -126,7 +126,7 @@ export default function TabLayout() {
         }}
       >
         <Tabs.Screen
-        name="community"
+        name="Community"
         options={{
           tabBarIcon: ({ focused }) => (
         <Image source={require("../../assets/images/Community.png")}
